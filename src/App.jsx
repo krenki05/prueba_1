df19efd956db625dc0692275e2698bdfb4a5fdae
Franco Casafus <francocasafus55@gmail.com>
2024-12-12 00:09:27 -0300

    adding home, navigation, abouy and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './components/home/Home'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Navbar from './components/navbar/Navbar'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <h1>Franco Casafus </h1>
-      </div>
+   <>
+   
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+
+    <div>
+      <h5>Franco Casafus</h5>
+    </div>
     </>
   )
 }
